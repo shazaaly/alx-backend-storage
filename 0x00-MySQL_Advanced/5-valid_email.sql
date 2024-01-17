e24494f2e178bb2b997a9923ed0fb93e64df0985
shazaaly <shaza.aly@gmail.com>
2024-01-17 23:13:54 +0200

    fix

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -5,8 +5,8 @@
 CREATE TRIGGER email_reset AFTER UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF OLD.email <> NEW.email THEN
-        UPDATE users SET NEW.valid_email = 0 WHERE id = NEW.id;
+    IF OLD.email != NEW.email THEN
+        UPDATE users SET NEW.valid_email = 0
     END IF;
 END;
 
