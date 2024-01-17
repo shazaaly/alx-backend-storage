af5a74d33279aa20ec4d6ec453c29d1ba8bd70c0
shazaaly <shaza.aly@gmail.com>
2024-01-17 18:56:58 +0200

    5-valid_email

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- Active: 1695137323808@@127.0.0.1@3306@holberton
+-- SQL script that creates a trigger that resets valid_email
+-- only when the email has been changed.
+
+CREATE TRIGGER email_reset AFTER UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email <> NEW.email THEN
+        UPDATE users SET valid_email = 0 WHERE id = NEW.id;
+    END IF;
+END;
+
