f59a559c8ca42df3cfad4f149324970a41672773
shazaaly <shaza.aly@gmail.com>
2024-01-18 13:34:07 +0200

    f2

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,7 +1,7 @@
 -- Script that creates a function SafeDiv that divides
 -- (and returns) the first by the second number or returns
 -- 0 if the second number is equal to 0.
-DELIMITER $$ ;
+DELIMITER $$
 CREATE FUNCTION SafeDiv(
 	a INT,
 	b INT
