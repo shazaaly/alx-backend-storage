f846ba5dd2f74f03a3b3e5a87ec210f44ab7e8a1
shazaaly <shaza.aly@gmail.com>
2024-01-22 23:24:29 +0200

    indent

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -10,14 +10,14 @@ if __name__ == "__main__":
     client = MongoClient('localhost', 27017)
     collection = client.logs.nginx
 
-all_logs = collection.count_documents({})
-get_logs = collection.count_documents({"method": "GET"})
-post_logs = collection.count_documents({"method": "POST"})
-put_logs = collection.count_documents({"method": "PUT"})
-patch_logs = collection.count_documents({"method": "PATCH"})
-delete_logs = collection.count_documents({"method": "DELETE"})
-checks = collection.count_documents({"method": "GET", "path": "/status"})
+    all_logs = collection.count_documents({})
+    get_logs = collection.count_documents({"method": "GET"})
+    post_logs = collection.count_documents({"method": "POST"})
+    put_logs = collection.count_documents({"method": "PUT"})
+    patch_logs = collection.count_documents({"method": "PATCH"})
+    delete_logs = collection.count_documents({"method": "DELETE"})
+    checks = collection.count_documents({"method": "GET", "path": "/status"})
 
-print("{} logs\n Methods:\n \t  method GET: {} \n\t  method POST: {} \n\t  method"
-          " PUT: {} \n\t  method PATCH: {} \n\t  method DELETE: {}"
-          "\n status_checks: {}".format(all_logs, get_logs, post_logs, put_logs, patch_logs, delete_logs, checks))
+    print("{} logs\n Methods:\n \t  method GET: {} \n\t  method POST: {} \n\t  method"
+              " PUT: {} \n\t  method PATCH: {} \n\t  method DELETE: {}"
+              "\n status_checks: {}".format(all_logs, get_logs, post_logs, put_logs, patch_logs, delete_logs, checks))
